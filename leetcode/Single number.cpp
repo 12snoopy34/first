0e94afa7439a965f84fd005b8665d7b54970c8eb
12snoopy34 <dongkn@hotmail.com>
2014-10-17 19:33:44 +0800

    leetcod

---
+++ leetcode/Single number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+        if(A==NULL||n==0)
+            return 0;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans^=A[i];
+        }
+        return ans;
+    }
+};
+
